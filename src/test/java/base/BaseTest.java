9e7caff783aeb25825fb1d36781257c6975bdcca
Bahaa.Alaa <bahaaa946@gmail.com>
2026-01-20 12:46:50 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	src/test/java/base/BaseTest.java

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -22,21 +22,23 @@ public class BaseTest {
         ConfigReader.loadConfig();
         String browser = ConfigReader.getProperty("browser");
         String url = ConfigReader.getProperty("url");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new"); // تشغيل بدون واجهة
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--window-size=1920,1080");
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-profile-" + Math.random()); // بيعمل بروفايل جديد لكل رن عشان ميتخانقوش مع بعض
 
         if (browser.equalsIgnoreCase("chrome")) {
             WebDriverManager.chromedriver().setup();
-            driver.set(new ChromeDriver());
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox"); // أساسي لجينكينز
+            options.addArguments("--disable-dev-shm-usage"); // بيحل مشكلة الـ Memory في Docker/Jenkins
+            options.addArguments("--disable-gpu");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--user-data-dir=" + System.getProperty("java.io.tmpdir") + "/chrome-profile-" + Math.random()); // بيعمل بروفايل جديد لكل رن عشان ميتخانقوش مع بعض
+            driver.set(new ChromeDriver(options));
         } else if (browser.equalsIgnoreCase("firefox")) {
             WebDriverManager.firefoxdriver().setup();
             driver.set(new FirefoxDriver());
         }
+
+        // ملاحظة: الـ maximize مش ضرورية في الـ headless بس سيبها مش هتضر
         getDriver().manage().window().maximize();
 
         if (url != null) {
@@ -53,7 +55,7 @@ public class BaseTest {
             System.out.println("✅ Browser closed and ThreadLocal reference removed.");
         }
     }*/
-  /*  @AfterSuite
+/*    @AfterSuite
     public void tearDownSuite() {
         String summary = "The automation run has finished. Please check the attached link for detailed results.";
         System.out.println("Sending Email Report...");
