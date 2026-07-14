39a1873c3ad463389df20fc3acd9c93545ca96c3
BahaaMohammed <bahaaa946@gmail.com>
2026-07-14 16:11:47 +0300

    done

--- Git/src/test/java/base/BaseTest.java
+++ Git/src/test/java/base/BaseTest.java
@@ -49,7 +49,8 @@ public class BaseTest {
             System.out.println("⚠️ URL is missing in config.properties!");
         }
     }
-/*    @AfterMethod
+
+    @AfterMethod
     public void teardown() {
         if (getDriver() != null) {
             getDriver().quit();
