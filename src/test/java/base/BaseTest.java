98cc3cc81ad89f3c5d9acbfd502f29f58de187df
BahaaMohammed <bahaaa946@gmail.com>
2026-07-14 13:19:30 +0300

    done

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
+import utils.EmailUtils;
 
 public class BaseTest {
     protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
