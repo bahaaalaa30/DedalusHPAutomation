86ef6d4eee7371364660ae711805d44daefce81d
BahaaMohammed <bahaaa946@gmail.com>
2026-07-14 16:05:28 +0300

    done

--- Git/src/main/java/pages/VisitBookingPage.java
+++ Git/src/main/java/pages/VisitBookingPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 import java.time.Duration;
 
 public class VisitBookingPage {
