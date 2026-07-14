af212fbe013e532fe5642186f5b88490254fd56d
BahaaMohammed <bahaaa946@gmail.com>
2026-07-14 13:19:36 +0300

    done

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import utils.ConfigReader;
 import utils.EmailUtils;
