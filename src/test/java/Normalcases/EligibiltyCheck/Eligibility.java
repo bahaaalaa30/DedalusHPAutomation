73a8e867b5a9d9649dac4c5e2added52ef969d4a
BahaaMohammed <bahaaa946@gmail.com>
2026-07-14 10:39:40 +0300

    Change Code Structure

--- src/test/java/Normalcases/EligibiltyCheck/Eligibility.java
+++ src/test/java/Normalcases/EligibiltyCheck/Eligibility.java
@@ -22,7 +22,7 @@ public class Eligibility extends BaseTest {
     public void checkEligibilityWithValidPID() {
         LoginPage loginPage = new LoginPage(getDriver());
         String url = ConfigReader.getProperty("PayerURL");
-        String PayerURLVisitPage = ConfigReader.getProperty("visitbookingurl");
+        String PayerURLVisitPage = ConfigReader.getProperty("payervisitbookingurl");
         System.out.println("🚀 Started: Valid Credentials Login Test");
         getDriver().get(url);
         loginPage.enterUsername(ConfigReader.getProperty("CMOB6"));
