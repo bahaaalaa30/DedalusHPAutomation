7a48f576a6497874cceb1ec96346d2765d489400
bahaaalaa30 <134206393+bahaaalaa30@users.noreply.github.com>
2026-09-23 17:42:51 +0300

    style: format Playwright TypeScript files

--- src/fixtures/baseTest.ts
+++ src/fixtures/baseTest.ts
@@ -1,1 +1,13 @@
-import { test as base, expect } from '@playwright/test'; import { config } from '../config/config'; import { LoginPage } from '../pages/LoginPage'; type Fixtures = { loginPage: LoginPage }; export const test = base.extend<Fixtures>({ loginPage: async ({ page }, use) => use(new LoginPage(page)) }); export { expect, config };
+import { test as base, expect } from '@playwright/test';
+import { config } from '../config/config';
+import { LoginPage } from '../pages/LoginPage';
+
+type Fixtures = {
+  loginPage: LoginPage;
+};
+
+export const test = base.extend<Fixtures>({
+  loginPage: async ({ page }, use) => use(new LoginPage(page)),
+});
+
+export { expect, config };
