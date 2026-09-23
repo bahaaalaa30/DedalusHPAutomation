08ae31b8d34fb0dab9a684f75ca949ddf1dd2ab5
bahaaalaa30 <134206393+bahaaalaa30@users.noreply.github.com>
2026-09-23 13:55:21 +0300

    fix: initialize Playwright page objects after constructor

--- src/pages/PractitionerPage.ts
+++ src/pages/PractitionerPage.ts
@@ -1,16 +1,27 @@
 import { expect, Page } from '@playwright/test';
 
 export class PractitionerPage {
-  constructor(private readonly page: Page) {}
+  private readonly patient;
+  private readonly patientsCount;
+  private readonly newDocument;
+  private readonly startConsultationButton;
 
-  private patient = this.page.locator('div.col-patient div.patient-name p.secondary-text');
-  private patientsCount = this.page.locator('app-crm-quick-filters div:nth-child(2) div:nth-child(3) div').first();
-  private newDocument = this.page.locator('div.new-actions.pointer');
-  private startConsultationButton = this.page.locator('div.start-consult-dialog button.primary-button');
+  constructor(private readonly page: Page) {
+    this.patient = page.locator('div.col-patient div.patient-name p.secondary-text');
+    this.patientsCount = page.locator(
+      'app-crm-quick-filters div:nth-child(2) div:nth-child(3) div'
+    ).first();
+    this.newDocument = page.locator('div.new-actions.pointer');
+    this.startConsultationButton = page.locator(
+      'div.start-consult-dialog button.primary-button'
+    );
+  }
 
   async selectClinic() {
     await this.page.locator('#facility-menu').click();
-    await this.page.locator('#facility-menu-actions > div > div > div > div > div:nth-child(3)').click();
+    await this.page.locator(
+      '#facility-menu-actions > div > div > div > div > div:nth-child(3)'
+    ).click();
   }
 
   async selectPatient() {
