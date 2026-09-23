c51512ae3962fe29e7603710eb6186f5c9bb6b02
bahaaalaa30 <134206393+bahaaalaa30@users.noreply.github.com>
2026-09-23 17:43:32 +0300

    style: format Playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,19 +1,36 @@
 import { defineConfig, devices } from '@playwright/test';
-export default defineConfig(
+
+export default defineConfig({
+  testDir: './src/tests',
+  timeout: 60000,
+  expect: {
+    timeout: 15000,
+  },
+  reporter: [
+    ['list'],
+    ['html', { open: 'never' }],
+    ['allure-playwright'],
+  ],
+  use: {
+    headless: process.env.HEADLESS !== 'false',
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure',
+    actionTimeout: 15000,
+    navigationTimeout: 30000,
+  },
+  projects: [
     {
-        testDir: './src/tests',
-        timeout: 60000,
-        expect: { timeout: 15000 },
-        reporter: [['list'],
-            ['html', { open: 'never' }],
-            ['allure-playwright']],
-        use: { headless: process.env.HEADLESS !== 'false',
-            screenshot: 'only-on-failure',
-            video: 'retain-on-failure',
-            trace: 'retain-on-failure',
-            actionTimeout: 15000, navigationTimeout: 30000 },
-        projects:
-            [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
-                /*{ name: 'firefox', use: { ...devices['Desktop Firefox'] } }] */
-    }
-    );
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome'],
+      },
+    },
+    // {
+    //   name: 'firefox',
+    //   use: {
+    //     ...devices['Desktop Firefox'],
+    //   },
+    // },
+  ],
+});
