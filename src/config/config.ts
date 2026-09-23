026e15bbd7b36926159fc7a3d29811bf2b4bbb58
bahaaalaa30 <134206393+bahaaalaa30@users.noreply.github.com>
2026-09-23 15:01:49 +0300

    fix: use correct CMO credential configuration

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,12 +6,12 @@ export const config = {
   generalPractitionerUrl: env('GP_URL', 'http://10.24.13.10/healthplug/#/user/leads'),
   payerUrl: env('PAYER_URL', 'http://100.74.6.62/payer/#/login'),
   payerVisitBookingUrl: env('PAYER_VISIT_BOOKING_URL', 'http://100.74.6.62/payer/#/user/clinicaldiary'),
-  username: env('CMOb6'),
-  password: env('egy123'),
+  username: env('CMOB6', 'cmob6'),
+  password: env('CMO_PASSWORD', 'egy123'),
   genUser: env('genb6', 'genb6'),
   genPass: env('123'),
   cmoB6: env('CMOB6', 'cmob6'),
-  cmoPassword: env('egy123'),
+  cmoPassword: env('CMO_PASSWORD', 'egy123'),
   elNasrDoctorUser: env('P00250', 'P00250'),
   elNasrDoctorPassword: env('egy123')
 };
