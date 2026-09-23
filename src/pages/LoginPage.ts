13b223d2f259d007786c616b6d5891b52d290f30
bahaaalaa30 <134206393+bahaaalaa30@users.noreply.github.com>
2026-09-23 15:09:42 +0300

    test: add login form diagnostics and Angular input events

--- src/pages/LoginPage.ts
+++ src/pages/LoginPage.ts
@@ -17,6 +17,8 @@ export class LoginPage {
     await this.username.waitFor({ state: 'visible', timeout: 5000 });
     await this.username.fill('');
     await this.username.pressSequentially(user, { delay: 20 });
+    await this.username.dispatchEvent('input');
+    await this.username.dispatchEvent('change');
     return this;
   }
 
@@ -24,11 +26,32 @@ export class LoginPage {
     await this.password.waitFor({ state: 'visible', timeout: 5000 });
     await this.password.fill('');
     await this.password.pressSequentially(pass, { delay: 20 });
+    await this.password.dispatchEvent('input');
+    await this.password.dispatchEvent('change');
     return this;
   }
 
   async clickLogin() {
     await this.loginButton.waitFor({ state: 'visible', timeout: 15000 });
+
+    const usernameValue = await this.username.inputValue();
+    const passwordLength = (await this.password.inputValue()).length;
+    const disabled = await this.loginButton.isDisabled();
+
+    console.log('[LOGIN] username:', usernameValue);
+    console.log('[LOGIN] password length:', passwordLength);
+    console.log('[LOGIN] button disabled:', disabled);
+    console.log('[LOGIN] username outerHTML:', await this.username.evaluate(el => el.outerHTML));
+    console.log('[LOGIN] password outerHTML:', await this.password.evaluate(el => el.outerHTML));
+    console.log('[LOGIN] button outerHTML:', await this.loginButton.evaluate(el => el.outerHTML));
+
+    if (disabled) {
+      await this.password.press('Tab');
+      await this.page.waitForTimeout(1000);
+
+      console.log('[LOGIN] button disabled after blur:', await this.loginButton.isDisabled());
+    }
+
     await expect(this.loginButton).toBeEnabled({ timeout: 15000 });
     await this.loginButton.click();
   }
