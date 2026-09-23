dcd4ab28b900ab22fb54abaef90f5af7cb7175e8
BahaaMohammed <bahaaa946@gmail.com>
2026-09-23 17:31:51 +0300

    Change Code Structure

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,2 +1,19 @@
 import { defineConfig, devices } from '@playwright/test';
-export default defineConfig({ testDir: './src/tests', timeout: 60000, expect: { timeout: 15000 }, reporter: [['list'], ['html', { open: 'never' }], ['allure-playwright']], use: { headless: process.env.HEADLESS !== 'false', screenshot: 'only-on-failure', video: 'retain-on-failure', trace: 'retain-on-failure', actionTimeout: 15000, navigationTimeout: 30000 }, projects: [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }, { name: 'firefox', use: { ...devices['Desktop Firefox'] } }] });
+export default defineConfig(
+    {
+        testDir: './src/tests',
+        timeout: 60000,
+        expect: { timeout: 15000 },
+        reporter: [['list'],
+            ['html', { open: 'never' }],
+            ['allure-playwright']],
+        use: { headless: process.env.HEADLESS !== 'false',
+            screenshot: 'only-on-failure',
+            video: 'retain-on-failure',
+            trace: 'retain-on-failure',
+            actionTimeout: 15000, navigationTimeout: 30000 },
+        projects:
+            [{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }]
+                /*{ name: 'firefox', use: { ...devices['Desktop Firefox'] } }] */
+    }
+    );
