b0380a44267634a5ecbdcc217269c71201eaacf4
BahaaMohammed <bahaaa946@gmail.com>
2026-09-23 14:10:30 +0300

    Change Code Structure

--- src/config/config.ts
+++ src/config/config.ts
@@ -6,12 +6,12 @@ export const config = {
   generalPractitionerUrl: env('GP_URL', 'http://10.24.13.10/healthplug/#/user/leads'),
   payerUrl: env('PAYER_URL', 'http://100.74.6.62/payer/#/login'),
   payerVisitBookingUrl: env('PAYER_VISIT_BOOKING_URL', 'http://100.74.6.62/payer/#/user/clinicaldiary'),
-  username: env('HP_USERNAME'),
-  password: env('HP_PASSWORD'),
-  genUser: env('GEN_USER', 'genb6'),
-  genPass: env('GEN_PASS'),
-  cmoB6: env('CMO_B6', 'cmob6'),
-  cmoPassword: env('CMO_PASSWORD'),
-  elNasrDoctorUser: env('ELNASR_USER', 'P00250'),
-  elNasrDoctorPassword: env('ELNASR_PASSWORD')
+  username: env('CMOb6'),
+  password: env('egy123'),
+  genUser: env('genb6', 'genb6'),
+  genPass: env('123'),
+  cmoB6: env('CMOB6', 'cmob6'),
+  cmoPassword: env('egy123'),
+  elNasrDoctorUser: env('P00250', 'P00250'),
+  elNasrDoctorPassword: env('egy123')
 };
